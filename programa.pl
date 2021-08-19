f1e1a4623c1c8c764bc7d69ee5afc5d3a440821b
Facundo Agustín Fernández <facuassain@gmail.com>
2021-08-19 10:31:01 -0300

    adictiva/1 terminada
    modelado de las redes sociales mejorado. En canal, ahora las redes sociales son 'cosas' específicas y no functores para que adictiva/1 pueda funcionar correctamente, y nos ahorramos el usar polimorfismo de manera innecesaria al momento de calcular los seguidores de un usuario

--- programa.pl
+++ programa.pl
@@ -1,24 +1,27 @@
 % Apellido y Nombre: Fernández Facundo Agustín
 % Legajo: 168081-0
 
-canal(ana,youtube(3000000)).
-canal(ana,instagram(2700000)).
-canal(ana,tikTok(1000000)).
-canal(ana,twitch(2)).
-canal(beto,twitch(120000)).
-canal(beto,youtube(6000000)).
-canal(beto,instagram(1100000)).
-canal(cami,tikTok(2000)).
-canal(dani,youtube(1000000)).
-canal(evelyn,instagram(1)).
+canal(ana,youtube,3000000).
+canal(ana,instagram,2700000).
+canal(ana,tikTok,1000000).
+canal(ana,twitch,2).
+canal(beto,twitch,120000).
+canal(beto,youtube,6000000).
+canal(beto,instagram,1100000).
+canal(cami,tikTok,2000).
+canal(dani,youtube,1000000).
+canal(evelyn,instagram,1).
 
-redSocial(youtube(_)).
-redSocial(instagram(_)).
-redSocial(tikTok(_)).
-redSocial(twitch(_)).
+% redSocial(youtube(_)).
+% redSocial(instagram(_)).
+% redSocial(tikTok(_)).
+% redSocial(twitch(_)).
+
+redSocial(RedSocial):-
+    distinct(RedSocial,canal(_,RedSocial,_)).
 
 usuario(Usuario):-
-    canal(Usuario,_).
+    canal(Usuario,_,_).
 
 % PUNTO 2) A)
 influencer(Usuario):-
@@ -29,29 +32,22 @@ influencer(Usuario):-
 totalSeguidores(Usuario,TotalSeguidores):-
     findall(
         Seguidores,
-        seguidoresDe(Usuario,Seguidores),
+        canal(Usuario,_,Seguidores),
         TodosLosSeguidores
     ),
     sum_list(TodosLosSeguidores, TotalSeguidores).
 
-seguidoresDe(Usuario,Seguidores):-
-    canal(Usuario,RedSocial),
-    seguidoresSegun(RedSocial,Seguidores).
 
-seguidoresSegun(youtube(Seguidores),Seguidores).
-seguidoresSegun(instagram(Seguidores),Seguidores).
-seguidoresSegun(tikTok(Seguidores),Seguidores).
-seguidoresSegun(twitch(Seguidores),Seguidores).
 
 % PUNTO 2) B)
 omnipresente(Usuario):-
     usuario(Usuario),
-    forall(redSocial(RedSocial),canal(Usuario,RedSocial)).
+    forall(redSocial(RedSocial),canal(Usuario,RedSocial,_)).
 
 % PUNTO 2) C)
 exclusivo(Usuario):-
-    canal(Usuario,RedSocial),
-    not((canal(Usuario,OtraRedSocial), RedSocial \= OtraRedSocial)).
+    canal(Usuario,RedSocial,_),
+    not((canal(Usuario,OtraRedSocial,_), RedSocial \= OtraRedSocial)).
 
 % PUNTO 3)
 
@@ -63,12 +59,13 @@ contenido(cami,twitch,stream(leagueOfLegends)).
 contenido(cami,youtube,video([cami],5)).
 contenido(evelyn,instagram,foto([evelyn,cami])).
 
+
 tematica(juego,leagueOfLegends).
 tematica(juego,minecraft).
 tematica(juego,aoe).
 
 adictiva(RedSocial):-
-    canal(_,RedSocial),
+    redSocial(RedSocial),
     forall(
         contenido(_,RedSocial,Contenido),
         contenidoAdictivo(Contenido)
@@ -83,5 +80,4 @@ contenidoAdictivo( stream(Tematica) ):-
 contenidoAdictivo( foto(Participantes) ):-
     length(Participantes, CantidadParticipantes),
     CantidadParticipantes < 4.
-    
 
