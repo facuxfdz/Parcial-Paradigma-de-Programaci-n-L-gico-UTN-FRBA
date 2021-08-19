4356cc219a0ad03d010685aba944ef2e0f12a39d
Facundo Agustín Fernández <facuassain@gmail.com>
2021-08-19 09:32:38 -0300

    Punto 1) a) terminado con algunos tests

--- programa.pl
+++ programa.pl
@@ -1,2 +1,48 @@
 % Apellido y Nombre: Fernández Facundo Agustín
 % Legajo: 168081-0
+
+canal(ana,youtube(3000000)).
+canal(ana,instagram(2700000)).
+canal(ana,tikTok(1000000)).
+canal(ana,twitch(2)).
+canal(beto,twitch(120000)).
+canal(beto,youtube(6000000)).
+canal(beto,instagram(1100000)).
+canal(cami,tikTok(2000)).
+canal(dani,youtube(1000000)).
+canal(evelyn,instagram(1)).
+
+usuario(Usuario):-
+    canal(Usuario,_).
+
+influencer(Usuario):-
+    usuario(Usuario), % Tenemos que ligar la variable antes de llegar a totalSeguidores puesto que este no es inversible
+    totalSeguidores(Usuario,TotalSeguidores),
+    TotalSeguidores > 10000.
+
+totalSeguidores(Usuario,TotalSeguidores):-
+    findall(
+        Seguidores,
+        seguidoresDe(Usuario,Seguidores),
+        TodosLosSeguidores
+    ),
+    sum_list(TodosLosSeguidores, TotalSeguidores).
+
+seguidoresDe(Usuario,Seguidores):-
+    canal(Usuario,RedSocial),
+    seguidoresSegun(RedSocial,Seguidores).
+
+seguidoresSegun(youtube(Seguidores),Seguidores).
+seguidoresSegun(instagram(Seguidores),Seguidores).
+seguidoresSegun(tikTok(Seguidores),Seguidores).
+seguidoresSegun(twitch(Seguidores),Seguidores).
+
+:- begin_tests(punto_1_a).
+
+test(usuario_con_mas_de_10000_seguidores_es_influencer):-
+    influencer(dani).
+
+test(usuario_con_menos_de_10000_seguidores_no_es_influencer, fail):-
+    influencer(evelyn).
+
+:- end_tests(punto_1_a).
